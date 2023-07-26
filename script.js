896853515db44e476f1f503ffe4544d704b7f715
VHugo632 <113177124+VHugo632@users.noreply.github.com>
2023-07-26 23:31:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.body
+const annually_text = document.getElementById("annually_text")
+const monthly_text = document.getElementById("monthly_text")
+const toggle_button = document.getElementById("toggle_button")
+const card_prices = document.getElementsByClassName("card_prices")
+
+let activated = false
+
+toggle_button.addEventListener("click", function(){
+    body.classList.toggle("toggle_activated")
+    if (activated == false) {
+        activated = true
+        annually_text.style.color = "hsl(237, 63%, 64%)"
+        setTimeout(function() {
+            annually_text.style.color = "hsl(234, 14%, 74%)"
+        }, 1000)
+        card_prices[0].innerHTML = "<span>$</span>199.99"
+        card_prices[1].innerHTML = "<span>$</span>249.99"
+        card_prices[2].innerHTML = "<span>$</span>399.99"
+    }
+    else {
+        activated = false
+        monthly_text.style.color = "hsl(237, 63%, 64%)"
+        setTimeout(function() {
+            monthly_text.style.color = "hsl(234, 14%, 74%)"
+        }, 1000)
+        card_prices[0].innerHTML = "<span>$</span>19.99"
+        card_prices[1].innerHTML = "<span>$</span>24.99"
+        card_prices[2].innerHTML = "<span>$</span>39.99"
+    }
+})
